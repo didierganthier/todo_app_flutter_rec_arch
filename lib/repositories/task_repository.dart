e08a80459272618c506230bc9878eeee338861e9
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-02-04 10:09:22 -0500

    Implement TaskRepository with SQLite database integration for task management

--- lib/repositories/task_repository.dart
+++ lib/repositories/task_repository.dart
@@ -1,1 +1,55 @@
-class TaskRepository {}
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:todo_app_flutter_rec_arch/models/task_model.dart';
+
+class TaskRepository {
+  Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  Future<Database> _initDB() async {
+    final dir = await getApplicationDocumentsDirectory();
+    final path = join(dir.path, 'tasks.db');
+
+    return openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            isCompleted INTEGER NOT NULL
+          )
+        ''');
+      },
+    );
+  }
+
+  Future<List<Task>> getTasks() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('tasks');
+    return maps.map((task) => Task.fromJson(task)).toList();
+  }
+
+  Future<int> addTask(Task task) async {
+    final db = await database;
+    return await db.insert('tasks', task.toJson());
+  }
+
+  Future<int> updateTask(Task task) async {
+    final db = await database;
+    return await db
+        .update('tasks', task.toJson(), where: 'i d= ?', whereArgs: [task.id]);
+  }
+
+  Future<int> deleteTask(int id) async {
+    final db = await database;
+    return await db.delete('tasks', where: 'id = ?', whereArgs: [id]);
+  }
+}
